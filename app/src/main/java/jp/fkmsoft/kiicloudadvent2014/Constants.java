4692fb7eaa18a818c553fddd3726f06d647d447d
hrk fkm <fkm@fkmsoft.jp>
2014-12-08 13:24:41 +0900

    fix constants

--- app/src/main/java/jp/fkmsoft/kiicloudadvent2014/Constants.java
+++ app/src/main/java/jp/fkmsoft/kiicloudadvent2014/Constants.java
@@ -7,4 +7,7 @@ public class Constants {
     public static final String APP_ID = "<input your app id>";
     public static final String APP_KEY = "<input your app key>";
     public static final String BASE_URL = "https://api-jp.kii.com/api";
+
+    public static final String PREF_NAME = "info";
+    public static final String PREF_KEY_TOKEN = "token";
 }
